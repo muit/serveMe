f11cf46e2b151521c59d012e1b7b1d74be54195b
muit <miguel_3c@hotmail.com>
2014-11-07 21:47:08 +0100

    0.2.48 - More Tests & Fixes to sessions

--- test/serveme_tests.js
+++ test/serveme_tests.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var expect = require('expect.js');
+var urlParser = require('url');
 
 describe('ServeMe HttpServer',function(){
     var ServeMe;
@@ -32,20 +33,97 @@ describe('ServeMe Routes',function(){
         done();
     });
 
+    it('adds a route', function(done){
+        var name = "/testadd",
+        callback = function(){
+            return "added a route";
+        };
+        ServeMe.Routes.add(name, callback);
+
+        expect(ServeMe.Routes.hashIds[name]).to.be(callback);
+        done();
+    });
+
+    it('gets an uncreated route like undefined', function(done){
+        var name = "/testget";
+
+        expect(ServeMe.Routes.get(name)).to.be(undefined);
+        done();
+    });
+
+    it('gets a created route', function(done){
+        var name = "/testget",
+        callback = function(){
+            return "got a route";
+        };
+        ServeMe.Routes.add(name, callback);
+
+        expect(ServeMe.Routes.get(name)).to.be(callback);
+        done();
+    });
+
     it('says Hello!', function(done)
     {
-        var port = 3000;
-        ServeMe.start(port);
+        ServeMe.start(3000);
 
         ServeMe.Routes.add("/hello",function(){
             return "Hello World";
         });
 
-        request.get('http://localhost:'+port+'/hello', function (err, res, body){
+        request.get('http://localhost:'+3000+'/hello', function (err, res, body){
             expect(res.statusCode).to.equal(200);
             expect(res.body).to.equal("Hello World");
             done();
         });
+    });
+
+    after(function ()
+    {
+        ServeMe.stop();
+    });
+});
+
+describe('ServeMe Sessions',function(){
+    var ServeMe;
+
+    before(function(done)
+    {
+        ServeMe = require("..")({
+            session:
+            {
+                enabled: true,
+                persistence: false,
+                lifetime: 86400
+            }
+        });
+        done();
+    });
+
+    it('exists', function(done)
+    {
+        expect(ServeMe.Session).not.to.be(undefined);
+        done();
+    });
+
+    it('creates a session', function(done){
+        var session = new ServeMe.Session(0, {
+            path:   "/session/login",
+            domain: "localhost"
+        });
+        expect(session).to.be.a(ServeMe.Session);
+
+        done();
+    });
+
+    it('created sessions contains correct arguments', function(done){
+        var session = new ServeMe.Session(0, {
+            path:   "/session/login",
+            domain: "localhost"
+        });
+        expect(session.path).to.be("/session/login");
+        expect(session.domain).to.be("localhost");
+
+        done();
     });
 
     after(function ()
