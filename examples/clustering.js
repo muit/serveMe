8f2d0e45654fb37af4767a30d1a472ae6ab1699a
muit <miguel_3c@hotmail.com>
2014-11-23 00:12:59 +0100

    0.4.3 - Cluster structure improved & Example

---
+++ examples/clustering.js
@@ -0,0 +1,51 @@
+/*************************
+ *Clustering Example
+*************************/
+
+//Require serve-me package
+var ServeMe = require('..');
+
+//*******************************
+// HTTP SERVER
+// Only server the html & other files
+//*******************************
+var port = 3000;
+var options = {
+    home: "index.html",
+    directory: "./examples/public",
+    debug: false,
+    log: true,
+
+    cluster: {
+        //Enabling clustering
+        enabled: true,
+
+        cpus: "max", //(Optional, Default: "max") Number of cpus of the cluster
+        auto_reload: true //(Optional, Default: true) Set it to true to reload cluster workers if died
+    }
+};
+
+//ATENTION: Cluster functionality stills in development. Its stability is limited.
+
+//Start the Server
+ServeMe = ServeMe(options, port);
+ServeMe.start();
+
+//Route example
+ServeMe.Routes.add("/hello", function()
+{
+    return "hello world!";
+});
+
+//Event example
+ServeMe.on("http_request", function()
+{
+    console.log("Hey! One more friend...");
+});
+
+//New session event
+ServeMe.on("new_session", function()
+{
+    console.log("Hey! One more friend...");
+    return true;
+});
