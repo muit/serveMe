724ef34f4f534eea3bc111b7d3d2c7781764603c
muit <miguel_3c@hotmail.com>
2015-01-05 23:33:42 +0100

    0.6.2-a - Fixing Sessions 1/2

--- examples/sessions.js
+++ examples/sessions.js
@@ -15,30 +15,31 @@ var options = {
         enabled: true,
         //OPTIONAL:
         //Session url - Sessions will be created when any client visit this url.
-        new_session_url: "/session"
+        new_session_url: "/login"
     }
 };
 // Load ServeMe
 ServeMe = ServeMe(options, port);
 
-var user     = "bear",
+var username = "bear",
     password = "drowssap";
 
 
 ServeMe.on("new_session", function(evt)
 {
+    var session = evt.session;
     // Will be called each new session petition reaches.
     ServeMe.log("\nNew user...");
-
+    ServeMe.log(session.data);
     // if user is correct & password too
-    if(user == session.data.user && password == evt.session.data.password)
+    if(username == session.data.username && password == session.data.password)
     {
-        ServeMe.log("  "+user+" has logged in.\n");
-        return "logged in";// return true or a string to accept the new session
+        ServeMe.log("  "+session.data.username+" has logged in.\n");
+        return "Happy to see you "+ session.data.username;// return true or a string to accept the new session
         //The string returned will be the response data.
     }
     // else return false (or nothing)
-    ServeMe.log("  Couldn´t log in.\n");
+    ServeMe.log("  Couldn't log in.\n");
     return false;
 });
 /**A session will be created visiting that url:
@@ -51,10 +52,12 @@ ServeMe.on("new_session", function(evt)
 
 ServeMe.on("session", function(evt)
 {
+    var session = evt.session;
     // Will be called each existing session enters.
 
     // If you recharge the webpage before, this message will be printed.
-    ServeMe.log("\n  "+evt.session.data.user+" entered again!");
+    ServeMe.log("\n  "+session.data.username+" entered again!");
+    return "Hi again, "+session.data.username;
 });
 
 // Start the server
